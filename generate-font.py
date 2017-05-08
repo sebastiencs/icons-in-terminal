a7d74560364b76252d3534cc84d1e392aa807d64
Sebastien Chapuis <sebastien@chapu.is>
2017-05-09 02:36:12 +0800

    Format the output + rename glyph with default name

--- generate-font.py
+++ generate-font.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import string
 import os
 import sys
 import json
@@ -12,15 +13,18 @@ FONT_EM = 1024
 POWERLINE_START = 0xE0A0
 POWERLINE_END = 0xE0D5
 
-
+# Insert the powerline glyphs at the same place that all the patched powerline fonts to make
+# our font compatible with most of plugins/modules that insert those glyphs
 def insert_powerline_extra(dest):
     codepoint = POWERLINE_START
     font = fontforge.open("./fonts/PowerlineExtraSymbols.otf")
     font.em = FONT_EM
+    excludes = [ 0xE0A4, 0xE0A5, 0xE0A6, 0xE0A7, 0xE0A8, 0xE0A9, 0xE0AA, 0xE0AB,
+                 0xE0AC, 0xE0AD, 0xE0AE, 0xE0AF, 0xE0C9, 0xE0CB, 0xE0D3 ]
     inserted = []
 
     while codepoint < POWERLINE_END:
-        if codepoint >= 0xE0A4 and codepoint <= 0xE0AF:
+        if codepoint in excludes:
             codepoint += 1
             continue
         font.selection.select(codepoint)
@@ -29,7 +33,27 @@ def insert_powerline_extra(dest):
         dest.paste()
         inserted.append(codepoint)
         codepoint += 1
-    print ("powerline-extras:" + hex(POWERLINE_START) + "-" + hex(POWERLINE_END))
+    print("#powerline-extras:" + str(len(inserted)))
+    for x in inserted:
+        print("powerline:" + str(hex(x)).replace("0x", ""), end=';')
+    print("")
+
+# Return true if the format of the name is something like 'uniXXXX' where X are
+# hexadecimals
+def is_default_name(name):
+    if len(name) != 7:
+        return False
+    if not name.startswith("uni"):
+        return False
+    if not all(c in string.hexdigits for c in name[-4:]):
+        return False
+    return True
+
+# If the name is a default one, return a new default name corresponding to its codepoint
+def make_name(name, codepoint):
+    if is_default_name(name):
+        return "uni" + str(hex(codepoint)).replace("0x", "").upper()
+    return name
 
 if len(sys.argv) < 2:
     print("Give me a config file")
@@ -47,15 +71,10 @@ with open(sys.argv[1]) as config_file:
 
     codepoint = PUA_START
 
-    num_file = 0
-
     for json_file in config_data:
 
         font = fontforge.open(json_file["path"])
-
         font.em = FONT_EM
-
-        num_file += 1
 
         excludes = []
         if "excludes" in json_file:
@@ -74,6 +93,7 @@ with open(sys.argv[1]) as config_file:
             move_vertically = int(json_file["move-vertically"])
 
         start = codepoint
+        inserted = []
 
         for symbol in font.glyphs("encoding"):
             if codepoint == POWERLINE_START:
@@ -87,15 +107,18 @@ with open(sys.argv[1]) as config_file:
                 name = symbol.glyphname
                 font.selection.select(encoding)
                 font.copy()
-#                print (json_file["name"] + ": " + name + " : " + hex(codepoint))
                 dest.selection.select(codepoint)
                 dest.paste()
                 dest.transform(psMat.translate(0, move_vertically))
-                dest.createMappedChar(codepoint).glyphname = name
+                new_name = make_name(name, codepoint)
+                dest.createMappedChar(codepoint).glyphname = new_name
+                inserted.append((new_name ,codepoint))
                 codepoint += 1
 
-        print(json_file["name"] + ":" + hex(start) + "-" + hex(codepoint - 1))
-
+        print("#" + json_file["name"] + ":" + str(len(inserted)))
+        for (name, x) in inserted:
+            print(name + ":" + str(hex(x)).replace("0x", ""), end=';')
+        print("")
 
     dest.em = FONT_EM
     dest.fontname = "icons-in-terminal"
