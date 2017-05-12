f9aa1dda9b63361371ad1e105785c8b6890f5442
Sebastien Chapuis <sebastien@chapu.is>
2017-05-12 22:20:02 +0800

    install built files in ~/.local/share/icons-in-terminal

--- install.sh
+++ install.sh
@@ -10,11 +10,22 @@ then
 fi
 
 set -xe
+
 mkdir -p ~/.fonts
 cp ./build/icons-in-terminal.ttf ~/.fonts/
 mkdir -p ~/.config/fontconfig/conf.d
 cp ./sample/icons.conf ~/.config/fontconfig/conf.d/20-icons.conf
 fc-cache -fvr --really-force ~/.fonts
+
+DATA="${HOME}/.local/share"
+if [ -n "$XDG_DATA_HOME" ]; then
+    DATA=$XDG_DATA_HOME
+fi
+DATA="${DATA}/icons-in-terminal/"
+mkdir -p $DATA
+cp ./build/* $DATA
+
 set +xe
 
+echo -e "\nBuilt files copied in $DATA (just to keep them safe)"
 echo "Font successfully installed. Now start a new terminal and run print_icons.sh :)"
