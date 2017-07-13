545fa3758c3f2e04812b1ba1da384afeca78a2ed
Sebastien Chapuis <sebastien@chapu.is>
2017-07-13 20:43:21 +0800

    Script to make bash variables bind to the icon, not the codepoint
    This propably solves errors with olds bash ? #2

---
+++ scripts/inte_without_codepoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+filename="./build/mapping.txt"
+count=0
+
+name=""
+
+while read -r line
+do
+    if [ "${line:0:1}" = "#" ]
+    then
+	name_num=(${line//:/ })
+	name=${name_num[0]:1}
+    else
+	str=""
+	IFS=';' read -ra array_glyph <<< "$line"
+	for glyph in "${array_glyph[@]}"; do
+	    info=(${glyph//:/ })
+	    echo -e "export ${info[0]}=\u${info[1]}"
+	    # echo "export ${info[0]}=\$(echo -e \"\u${info[1]}\")"
+	done
+    fi
+
+done < "$filename"
